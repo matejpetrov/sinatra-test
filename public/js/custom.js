90d69fafca3153d3a8cbcca65bec9ecb846db43b
MATEJ PETROV <matejp@medialab.io>
2016-02-12 17:28:08 +0100

    feat: Implementing send email functionality

---
+++ public/js/custom.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){	
+	if(window.location.hash == "#success"){
+		$(".success").show().fadeTo( 8000 , 0)
+	}
+});
--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-
+require 'net/smtp'
+require 'yaml'
 
 class SinatraApp < Sinatra::Base
 	configure do	  
@@ -12,6 +13,62 @@ class SinatraApp < Sinatra::Base
 
 	get '/gallery' do
 		send_file 'gallery.html'
+	end
+
+	post '/sendmail' do
+		Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+		name = params["name"]
+		email_from = params["email"]
+		text = params["message"]
+
+		message = "D-Custom visitor #{name}, with #{email_from} as email address, sends this message \n\n #{text}"
+
+		username, password = email_credentials
+		redirect_url_value = redirect_url
+
+		msgstr = <<END_OF_MESSAGE
+Subject: D Custom Test Message
+Date: #{Time.now.rfc2822}
+From: #{email_from}
+
+#{message}
+END_OF_MESSAGE
+
+		to = "petrov_matej@yahoo.com"
+
+		Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', username, password, :plain) do |smtp|
+		  smtp.send_message msgstr, email_from, to
+		end
+
+		redirect redirect_url_value
+	end
+
+	private
+
+	def email_credentials
+		if self.class.development?
+			yaml = YAML.load_file('variables.yml')
+			[ yaml['email_credentials']['username'], yaml['email_credentials']['password'] ]
+		else
+			[ ENV['email_username'], ENV['email_password'] ]
+		end
+	end
+
+	def redirect_url
+		if self.class.development?
+			'http://localhost:9292#success'
+		else
+			'http://d-custom.herokuapp.com#success'
+		end
+	end
+
+	def dev_email_credentials
+		yaml = YAML.load_file('variables.yml')
+		[ yaml['email_credentials']['username'], yaml['email_credentials']['password'] ]
+	end
+
+	def prod_email_credentials
+		[ ENV['email_username'], ENV['email_password'] ]
 	end
 
 end
